f4cdbfea1799cb35366e2239f913246380834e6e
PlebExMachina <plebexmachina@gmail.com>
2021-03-26 20:30:59 -0700

    Implemented generic stat system. WIP: Cached stats for frequent access.

---
+++ Plugins/PCPP/Source/PCPP_Components/Private/RPGCore.cpp
@@ -0,0 +1,159 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RPGCore.h"
+
+// Sets default values for this component's properties
+URPGCore::URPGCore()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void URPGCore::BindStat(FName Name, FRPGStatConfig Config, EStatDefault Default) {
+	// Sanity check, make sure a stat isn't already bound.
+	auto ExistingDelegate = _Delegates.Find(Name);
+	if (ExistingDelegate) {
+		return;
+	}
+
+	// Determine default value.
+	float DefaultValue = 0.0;
+	switch (Default) {
+	case EStatDefault::Literal:
+		DefaultValue = Config.LiteralDefault;
+		break;
+	case EStatDefault::Minimum:
+		DefaultValue = GetStat(Config.MinimumConstraint);
+		break;
+	case EStatDefault::Maximum:
+		DefaultValue = GetStat(Config.MaximumConstraint);
+		break;
+	}
+
+	// Bind relevant information.
+	_Delegates.Add(Name, FStatUpdatedDelegate());
+	_Configs.Add(Name, Config);
+	_Statistics.Add(Name, DefaultValue);
+	_Dependencies.Add(Name, TSet<FName>());
+
+	// Setup dependencies.
+	if (Config.MaximumConstraint != NAME_None) {
+		auto DependencyArray = _Dependencies.Find(Config.MaximumConstraint);
+		if (DependencyArray) {
+			DependencyArray->Add(Name);
+		}
+	}
+	if (Config.MinimumConstraint != NAME_None) {
+		auto DependencyArray = _Dependencies.Find(Config.MinimumConstraint);
+		if (DependencyArray) {
+			DependencyArray->Add(Name);
+		}
+	}
+}
+
+void URPGCore::BindDerivedStat(FName Name, DerivedStatFunctionPtr Definition)
+{
+	_DerivedStatistics.Add(Name, Definition);
+}
+
+float URPGCore::GetDerivedStat(FName Name)
+{
+	// Ensure it exists.
+	auto DerivedStatistic = _DerivedStatistics.Find(Name);
+	if (!DerivedStatistic) {
+		return 0.f;
+	}
+	// Sanity check, someone may have set a nullptr. Got to avoid seg-faults!
+	if (!(*DerivedStatistic)) {
+		return 0.f;
+	}
+	// Return based on the provided function.
+	return (*DerivedStatistic)(_Statistics);
+}
+
+float URPGCore::GetStat(FName Name) {
+	auto Value = _Statistics.Find(Name);
+	if (!Value) {
+		return 0.f;
+	}
+	return *Value;
+}
+
+void URPGCore::SetStat(FName Name, float NewValue)
+{
+	auto Constraints = _Configs.Find(Name);
+	if (!Constraints) {
+		return;
+	}
+
+	// Enforce Constraints
+	if (Constraints->MaximumConstraint != NAME_None) {
+		if (NewValue > GetStat(Constraints->MaximumConstraint)) {
+			NewValue = GetStat(Constraints->MaximumConstraint);
+		}
+	}
+	if (Constraints->MinimumConstraint != NAME_None) {
+		if (NewValue < GetStat(Constraints->MinimumConstraint)) {
+			NewValue = GetStat(Constraints->MinimumConstraint);
+		}
+	}
+
+	float OldValue = GetStat(Name);
+	_Statistics.Add(Name, NewValue);
+	BroadcastDelegate(Name, OldValue);
+}
+
+void URPGCore::AddStat(FName Name, float NewValue)
+{
+	SetStat(Name, GetStat(Name) + NewValue);
+}
+
+void URPGCore::SetToMax(FName Name)
+{
+	auto Constraints = _Configs.Find(Name);
+	if (!Constraints) {
+		return;
+	}
+	if (Constraints->MaximumConstraint == NAME_None) {
+		return;
+	}
+
+	float OldValue = GetStat(Name);
+	_Statistics.Add(Name, GetStat(Constraints->MaximumConstraint));
+	BroadcastDelegate(Name, OldValue);
+}
+
+void URPGCore::SetToMin(FName Name)
+{
+	auto Constraints = _Configs.Find(Name);
+	if (!Constraints) {
+		return;
+	}
+	if (Constraints->MinimumConstraint == NAME_None) {
+		return;
+	}
+
+	float OldValue = GetStat(Name);
+	_Statistics.Add(Name, GetStat(Constraints->MinimumConstraint));
+	BroadcastDelegate(Name, OldValue);
+}
+
+void URPGCore::BroadcastDelegate(FName Key, float OldValue) {
+	float NewValue = GetStat(Key);
+	if (OldValue != NewValue) {
+		(_Delegates.Find(Key))->Broadcast(OldValue, NewValue);
+
+		// Propagate changes across those dependant.
+		auto Dependants = _Dependencies.Find(Key);
+		if (Dependants) {
+			for (auto It = Dependants->CreateIterator(); It; ++It) {
+				// Force those dependant to re-evaluate their constraints.
+				SetStat(*It, GetStat(*It));
+			}
+		}
+	}
+}
+
+FStatUpdatedDelegate* URPGCore::GetDelegate(FName Name) {
+	return _Delegates.Find(Name);
+}
---
+++ Plugins/PCPP/Source/PCPP_Components/Public/RPGCore.h
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RPGCore.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FStatUpdatedDelegate, float, OldValue, float, UpdatedValue);
+
+/*
+* The default mode for the stat.
+* Literal -> Use the config defined value.
+* Otherwise use the value saved in the minimum/maximum key.
+*/
+UENUM(BlueprintType)
+enum class EStatDefault : uint8 {
+	Literal,
+	Minimum,
+	Maximum,
+};
+
+USTRUCT(BlueprintType)
+struct FRPGStatConfig {
+	GENERATED_BODY();
+
+	// Default value if not overridden.
+	UPROPERTY(EditAnywhere)
+	float LiteralDefault;
+
+	// Sets the minimum value (as an existing stat) that a stat can be.
+	UPROPERTY(EditAnywhere)
+	FName MinimumConstraint;
+
+	// Sets that maximum value (as an existing stat) that a stat can be.
+	UPROPERTY(EditAnywhere)
+	FName MaximumConstraint;
+
+	// Bind Raw Pointer Access. Bind to delegate.
+	FRPGStatConfig() {
+		MinimumConstraint = NAME_None;
+		MaximumConstraint = NAME_None;
+		LiteralDefault = 0.f;
+	}
+};
+
+
+/*
+* Dynamically allocates RPG Statistics and provides listeners for their updates.
+* If neccessary the data can also be queried directly.
+*/
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PCPP_COMPONENTS_API URPGCore : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	URPGCore();
+
+private:
+	void BroadcastDelegate(FName Key, float OldValue);
+
+protected:
+
+	// The actual value of the stat.
+	TMap<FName, float > _Statistics;
+
+	// Configuration data for each stat.
+	TMap<FName, FRPGStatConfig> _Configs;
+
+	// Delegates used to broadcast changes made to stats.
+	TMap<FName, FStatUpdatedDelegate> _Delegates;
+
+	// Keeps track of stats that are directly dependant on the stat in question.
+	TMap<FName, TSet<FName>> _Dependencies;
+
+	typedef float(*DerivedStatFunctionPtr)(const TMap<FName, float >&);
+
+	// Keeps track of derived stat
+	TMap<FName, DerivedStatFunctionPtr> _DerivedStatistics;
+
+public:
+
+	/*
+	* Binds a stat setting up the appropriate constraints.
+	* Be aware that order matters. For example you will end up with 0 if you define CurrentHP before MaxHP and set the default to MaxHP.
+	* Dependies may not also be set correctly if order is not adhered to.
+	*/
+	UFUNCTION(BlueprintCallable)
+		void BindStat(FName Name, FRPGStatConfig Config, EStatDefault Default = EStatDefault::Literal);
+
+	/* C++ Only, (Blueprint can still Get).
+	* Typical usage is to bind a lambda that takes in a "const TMap<FName, float >&" with no captures.
+	* Will bind a stat that will be based on existing stats.
+	* Nothing is actually stored so constraints of any kind are not respected.
+	*/
+	void BindDerivedStat(FName Name, DerivedStatFunctionPtr Definition);
+
+	UFUNCTION(BlueprintPure)
+	float GetDerivedStat(FName Name);
+
+	// Gets the value of a bound stat.
+	UFUNCTION(BlueprintPure)
+	float GetStat(FName Name);
+
+	// Sets the value, obeying constraints if they are set.
+	UFUNCTION(BlueprintCallable)
+	void SetStat(FName Name, float NewValue);
+
+	// Adds to the value, obeying constraints if they are set.
+	UFUNCTION(BlueprintCallable)
+	void AddStat(FName Name, float NewValue);
+
+	// Sets to the maximum if a constraint is set.
+	UFUNCTION(BlueprintCallable)
+	void SetToMax(FName Name);
+
+	// Sets to the minimum if a constraint is set.
+	UFUNCTION(BlueprintCallable)
+	void SetToMin(FName Name);
+
+	// Gets an existing delegate. Returns nullptr on failure.
+	FStatUpdatedDelegate* GetDelegate(FName Name);
+};
+
+USTRUCT(BlueprintType)
+struct FRPGCachedData {
+	GENERATED_BODY()
+
+};
