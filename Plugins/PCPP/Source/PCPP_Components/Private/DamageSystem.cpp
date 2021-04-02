f629cfcfa675982962fb8600048d3aa82ec82d1d
PlebExMachina <plebexmachina@gmail.com>
2021-04-01 21:00:19 -0700

    Damage System Implemented

--- Plugins/PCPP/Source/PCPP_Components/Private/DamageSystem.cpp
+++ Plugins/PCPP/Source/PCPP_Components/Private/DamageSystem.cpp
@@ -3,14 +3,20 @@
 
 #include "DamageSystem.h"
 
+TMap<FName, DamageFormula> UDamageSystem::_Formulas = {};
+
 // Sets default values for this component's properties
 UDamageSystem::UDamageSystem()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	OwnerRPGCore = nullptr;
+	SetComponentTickEnabled(false);
+	_DOTFormulas = {};
+	_DOTDuration = {};
+	_DOTMagnitude = {};
+	_DOTAttackers = {};
 }
 
 
@@ -18,9 +24,7 @@ UDamageSystem::UDamageSystem()
 void UDamageSystem::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	OwnerRPGCore = Cast<URPGCore>(GetOwner()->GetComponentByClass(URPGCore::StaticClass()));	
 }
 
 
@@ -28,7 +32,81 @@ void UDamageSystem::BeginPlay()
 void UDamageSystem::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (_DOTFormulas.Num() == 0) {
+		SetComponentTickEnabled(false);
+		return;
+	}
 
-	// ...
+	// Number of elements are synchronized as they're all initialized simultaniously.
+	// Likewise array indices will also be syncrhonized.
+	auto FormulaIterator = _DOTFormulas.CreateIterator();
+	auto DurationIterator = _DOTDuration.CreateIterator();
+	auto MagnitudeIterator = _DOTMagnitude.CreateIterator();
+	auto AttackerIterator = _DOTAttackers.CreateIterator();
+
+	while (FormulaIterator) {
+		// Very small dot time remaining case.
+		if ((*DurationIterator - DeltaTime) < 0.f) {
+			(*FormulaIterator)(*AttackerIterator, OwnerRPGCore, *DurationIterator, *MagnitudeIterator);
+		} else {
+		// Normal Case
+			(*FormulaIterator)(*AttackerIterator, OwnerRPGCore, DeltaTime, *MagnitudeIterator);
+		}
+
+
+		// Decrement Time Passed
+		*DurationIterator -= DeltaTime;
+
+		// Duration Expired Case
+		if (*DurationIterator < 0.f) {
+			FormulaIterator.RemoveCurrent();
+			DurationIterator.RemoveCurrent();
+			MagnitudeIterator.RemoveCurrent();
+			AttackerIterator.RemoveCurrent();
+		} else {
+		// Incrementing Case
+			++FormulaIterator;
+			++DurationIterator;
+			++MagnitudeIterator;
+			++AttackerIterator;
+		}
+	}
+}
+
+void UDamageSystem::RegisterDamageFormula(FName Name, DamageFormula Formula)
+{
+	_Formulas.Add(Name, Formula);
+}
+
+void UDamageSystem::TryInflictDamage(AActor * Other, FName Formula, float Magnitude, float Duration)
+{
+	if (OwnerRPGCore) {
+		auto FormulaInstance = _Formulas.Find(Formula);
+		if (FormulaInstance) {
+			auto TargetRPGCore = Cast<URPGCore>(Other->GetComponentByClass(URPGCore::StaticClass()));
+			if (TargetRPGCore) {
+				(*FormulaInstance)(OwnerRPGCore, TargetRPGCore, Magnitude, Duration);
+			}
+		}
+	}
+}
+
+void UDamageSystem::TryInflictDamageOverTime(AActor * Other, FName Formula, float Magnitude, float Duration)
+{
+	if (OwnerRPGCore) {
+		auto FormulaInstance = _Formulas.Find(Formula);
+		if (FormulaInstance) {
+			auto TargetSystem = Cast<UDamageSystem>(Other->GetComponentByClass(UDamageSystem::StaticClass()));
+
+			// Append to target's (private) DOT List and then begin DOT processing if it isn't already running.
+			if (TargetSystem) {
+				TargetSystem->_DOTAttackers.Add(OwnerRPGCore);
+				TargetSystem->_DOTDuration.Add(Duration);
+				TargetSystem->_DOTMagnitude.Add(Magnitude);
+				TargetSystem->_DOTFormulas.Add(*FormulaInstance);
+				TargetSystem->SetComponentTickEnabled(true);
+			}
+		}
+	}
 }
 
--- Plugins/PCPP/Source/PCPP_Components/Public/DamageSystem.h
+++ Plugins/PCPP/Source/PCPP_Components/Public/DamageSystem.h
@@ -4,9 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "RPGCore.h"
 #include "DamageSystem.generated.h"
 
+// Represent damage being inflicted from Attacker to Defender. Also carries an optional "magnitude" and "duration."
+typedef void (*DamageFormula)(URPGCore*, URPGCore*, float, float);
 
+/*
+* Damage System, allows for the storage of damage formulas which can interact between the owner and target's RPGCore components.
+* It uses "In to Out" based logic for damage flow where the Attacker's stats are Projected towards a Defender target.
+*/
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PCPP_COMPONENTS_API UDamageSystem : public UActorComponent
 {
@@ -15,14 +22,33 @@ class PCPP_COMPONENTS_API UDamageSystem : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UDamageSystem();
+private:
+	// Global Formula Storage.
+	static TMap<FName, DamageFormula> _Formulas;
 
+	// Active damage over time info. (Duration, Magnitude, Formula)
+	TArray<DamageFormula> _DOTFormulas;
+	TArray<float> _DOTDuration;
+	TArray<float> _DOTMagnitude;
+
+	// The attacker inflicting the DOT.
+	TArray<URPGCore*> _DOTAttackers;
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	URPGCore* OwnerRPGCore;
+
 public:	
-	// Called every frame
+	// Required for 'Damage over Time' based approaches.
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	// Registers a damage formula. C++ Only. I recommend loading them all at once while the game is starting up.
+	static void RegisterDamageFormula(FName Name,DamageFormula Formula);
+
+	UFUNCTION(BlueprintCallable)
+	void TryInflictDamage(AActor* Other, FName Formula = NAME_None, float Magnitude = 0.f, float Duration = 0.f);
+
+	UFUNCTION(BlueprintCallable)
+	void TryInflictDamageOverTime(AActor* Other, FName Formula = NAME_None, float Magnitude = 0.f, float Duration = 0.f);
 };
